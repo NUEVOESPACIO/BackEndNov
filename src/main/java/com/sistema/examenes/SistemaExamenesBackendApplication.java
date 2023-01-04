0bc6d7b5693031faba9b0607788a07774d721adc
NUEVOESPACIO <sebacp3@hotmail.com>
2023-01-04 12:07:37 -0300

    Agregando @EnabledSecurity en classe de arranque

--- src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
+++ src/main/java/com/sistema/examenes/SistemaExamenesBackendApplication.java
@@ -1,15 +1,17 @@
 package com.sistema.examenes;
 
-import com.sistema.examenes.entidades.Rol;
-import com.sistema.examenes.entidades.Usuario;
-import com.sistema.examenes.entidades.UsuarioRol;
+import com.sistema.examenes.security.JWTAuthorizationFilter;
 import com.sistema.examenes.servicios.UsuarioService;
-import java.util.HashSet;
-import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class SistemaExamenesBackendApplication implements CommandLineRunner {
@@ -30,29 +32,34 @@ public class SistemaExamenesBackendApplication implements CommandLineRunner {
         usuario.setPassword("12345");
         usuario.setEmail("c1a@gmail.com");
         usuario.setTelefono("9328434");
-        usuario.setPerfil("foto.png");
-        
+        usuario.setPerfil("foto.png");        
         Rol rol = new Rol();
         rol.setRolId(1L);
-        rol.setNombre("ADMIN");
-        
+        rol.setNombre("ADMIN");        
         Set<UsuarioRol> usuarioRoles = new HashSet<>();
         UsuarioRol usuarioRol=new UsuarioRol();
         usuarioRol.setRol(rol);
         usuarioRol.setUsuario(usuario);
-        usuarioRoles.add(usuarioRol);
-        
+        usuarioRoles.add(usuarioRol);        
         Usuario usuarioGuardado = usuarioService.guardarUsuario(usuario, usuarioRoles);
-        System.out.println(usuarioGuardado.getUsername());*/
-        
-        
-        
-        
-        
-        
-        
-        
+        System.out.println(usuarioGuardado.getUsername());*/        
     }
+    
+        @EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/user").permitAll()
+                                .antMatchers(HttpMethod.GET, "/tipoempleos/listar").permitAll()
+                                .antMatchers(HttpMethod.GET, "/*/*").permitAll()
+				.anyRequest().authenticated();
+		}
+	}
     
 
 }
